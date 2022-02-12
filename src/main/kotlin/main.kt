00e61f3d4ba3f5ae7acbf8cfe89286fac19366eb
Shamimgardobuya <shamimobuya@gmail.com>
2022-02-12 15:54:48 +0300

    first commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,26 @@
+
+
+fun main() {
+
+val studentName= ("Madai Puzo")
+    println(studentName)
+
+    val age= (22)
+    println(age)
+
+val phoneNumber= (250523436)
+    println(phoneNumber)
+
+    val weightKg: Float =63.45F
+    println(weightKg)
+
+
+
+
+   var kenyan : Boolean = false
+var notKenyan : Boolean = true
+    println(notKenyan)
+    }
+
+
+
